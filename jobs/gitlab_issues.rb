2d9c6cd4380b60292b562fb175dbf6edbb962e0c
Vinh Ngu <vinh-ngu@hotmail.com>
2019-03-02 18:33:38 +0100

    Add 'Gitlab-open-issues'-widget.

---
+++ jobs/gitlab_issues.rb
@@ -0,0 +1,32 @@
+require 'gitlab'
+
+# TODO: Move config to yaml
+Gitlab.configure do |config|
+  config.endpoint = ENV["GITLAB_HOST"] + '/api/v4'
+  config.private_token = ENV["GITLAB_PERSONAL_ACCESS_TOKEN"]
+end
+
+def get_open_issues(group_path, state)
+  my_group = Gitlab.groups(:search => group_path).find do |group|
+    group.path == group_path
+  end
+  projects = Gitlab.group(my_group.id).projects.map do |proj|
+    {:id => proj['id'], :name => proj['name']}
+  end
+
+  project_issues = Hash.new
+  projects.each do |proj|
+    project_issues[proj[:name]] = Gitlab.issues(proj[:id], :state => state).length
+  end
+  project_issues
+end
+
+SCHEDULER.every '5s', :first_in => 0 do |job|
+  open_issues = get_open_issues("curilab", "opened")
+  result = Array.new
+  open_issues.each do |project, issue_num|
+    result.push({value: issue_num, label: project})
+  end
+  result = result.sort_by {|result| result.zip}.reverse
+  send_event('gitlab-open-issues', {items: result})
+end
